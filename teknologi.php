5ab0ab3f6d038d913440f4c5e2fd91e1ed67d718
efati geovani <127381007+efatigeovaniisir@users.noreply.github.com>
2023-07-04 23:58:42 +0900

    Update teknologi.php

--- teknologi.php
+++ teknologi.php
@@ -17,13 +17,13 @@
 
     <!-- NAV START -->
     <nav class  ="border border-gray-800 container mx-auto p-6 text-center">
-        <a href="" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition" > Beranda</a>
-        <a href="" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Teknologi</a>
-        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Pendidikan</a>
-        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Pengembangan Diri</a>
-        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Kursus</a>
-        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Komunitas</a>
-        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Lainnya</a>
+        <a href="" class="border-dotted border-2 border-indigo-400 py-3 px-4 bg-blue-400 hover:bg-blue-600 transition" > Beranda</a>
+        <a href="" class="border-dotted border-2 border-indigo-400 py-3 px-4 bg-blue-400 hover:bg-blue-600 transition"> Teknologi</a>
+        <a href="" class="border-dotted border-2 border-indigo-400 py-3 px-4 bg-blue-400 hover:bg-blue-600 transition"> Pendidikan</a>
+        <a href="" class="border-dotted border-2 border-indigo-400 py-3 px-4 bg-blue-400 hover:bg-blue-600 transition"> Pengembangan Diri</a>
+        <a href=""  class="border-dotted border-2 border-indigo-400 py-3 px-4 bg-blue-400 hover:bg-blue-600 transition"> Kursus</a>
+        <a href=""  class="border-dotted border-2 border-indigo-400 py-3 px-4 bg-blue-400 hover:bg-blue-600 transition"> Komunitas</a>
+        <a href=""  class="border-dotted border-2 border-indigo-400 py-3 px-4 bg-blue-400 hover:bg-blue-600 transition"> Lainnya</a>
     </nav>
 
     <!-- NAV END -->
