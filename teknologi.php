7c709a1e4f7f20dab86d4b5765b8950f779cfd0b
efati geovani <127381007+efatigeovaniisir@users.noreply.github.com>
2023-07-04 23:59:59 +0900

    Update teknologi.php

--- teknologi.php
+++ teknologi.php
@@ -44,7 +44,7 @@
 
     <!-- FOOTER START -->
     <footer class="border border-gray-800 container mx-auto p-6 text-center">
-        <p> Copyright &copy 2023, SaCode Developed By <a href="#" class="font-semibold">Samuel</a> </p>
+        <p> Copyright &copy 2023, SaCode Developed By <a href="#" class="font-semibold">Efati </a> </p>
     </footer>
     <!-- FOOTER END -->
 
