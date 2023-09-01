16bf129b48a7f539a82600052397f1e3082a2c76
efati geovani <127381007+efatigeovaniisir@users.noreply.github.com>
2023-09-01 11:10:24 +0900

    add

--- include/header.php
+++ include/header.php
@@ -1,5 +1,5 @@
-<header class="container mx-auto border border-gray-800 p-6 bg-slate-200">
+<header class="container mx-auto border border-gray-800 p-6 bg-slate-200 mt-5">
 
-    <h1 class="font-bold text-6xl mb-3">SaCode new's</h1>
+    <h1 class="font-bold text-6xl mb-3 mt-3">SaCode new's</h1>
     <p>Berbagai berita terbaru seputar SaCode</p>
 </header>
